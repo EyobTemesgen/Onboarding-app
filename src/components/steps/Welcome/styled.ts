1c968d29838014f4e807d7d32e546c05aec3f8a8
Eyob Temesgen <111111244+EyobTemesgen@users.noreply.github.com>
2025-07-21 14:32:06 +0300

    Cleanups

--- src/components/steps/Welcome/styled.ts
+++ src/components/steps/Welcome/styled.ts
@@ -2,24 +2,6 @@ import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
 
 export const useWelcomeStyles = makeStyles<Theme>(() => ({
-  page: {
-    textAlign: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '12px',
-  },
-  logo: {
-    width: '64px',
-    height: '64px',
-    background: 'linear-gradient(to bottom right, #2563eb, #4338ca)',
-    borderRadius: '50%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '0 auto',
-    boxShadow:
-      '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-  },
   highlights: {
     display: 'grid',
     gridTemplateColumns: 'repeat(1, 1fr)',
